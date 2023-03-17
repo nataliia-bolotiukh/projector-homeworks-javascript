dcde94c052503ef72be1521e82525c1485342d84
nataliia-bolotiukh <126393745+nataliia-bolotiukh@users.noreply.github.com>
2023-03-17 10:22:13 +0100

    Fix

--- task-3.js
+++ task-3.js
@@ -3,17 +3,21 @@
 //   функцію яка ітеративно (в циклі) буде знаходити суму всіх непарних додатніх чисел до якогось числа.
 
 
+
 function recursiveOddSumTo(number) {
   if (number <= 0) {
     return 0;
-  } else if (number % 2 === 0) {
-    return recursiveOddSumTo(number - 1);
-  } else {
-    return number + recursiveOddSumTo(number - 2);
   }
+  
+  if (number % 2 === 0) {
+    return recursiveOddSumTo(number - 1);
+  }  
+  
+  return number + recursiveOddSumTo(number - 2);
+  
   };
-  console.log(recursiveOddSumTo(1)) 
-  console.log(recursiveOddSumTo(10)) 
+  console.log(recursiveOddSumTo(1)) // 1
+  console.log(recursiveOddSumTo(10)) // 25 
 
 
 
