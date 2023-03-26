d0567b2890fc7b62b30dc195f22aa7637ab51ea3
nataliia-bolotiukh <126393745+nataliia-bolotiukh@users.noreply.github.com>
2023-03-26 15:45:00 +0200

    Fix

--- lesson-7-functions/task-2.js
+++ lesson-7-functions/task-2.js
@@ -1,9 +1,5 @@
-function multiply(a) {
-  return function (b) {
-    return a * b
-  };
-}
+multiply = (a) => (b) => a * b;
 
-console.log(multiply(5)(5)) // 25*
-console.log(multiply(2)(-2)) // -4*
-console.log(multiply(4)(3)) // 12*
+console.log(multiply(5)(5)); // 25*
+console.log(multiply(2)(-2)); // -4*
+console.log(multiply(4)(3)); // 12*
