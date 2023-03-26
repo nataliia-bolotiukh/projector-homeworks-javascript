8ea84c98fb855599a851ba5a3d8086af72ea259f
nataliia-bolotiukh <126393745+nataliia-bolotiukh@users.noreply.github.com>
2023-03-26 15:13:30 +0200

    Fix

--- lesson-7-functions/task-1.js
+++ lesson-7-functions/task-1.js
@@ -1,10 +1,9 @@
-function addThemAll(...args) {
-  let sum = 0;
-
-  for (let arg of args) sum += arg;
-
-  return sum;
+function addThemAll(...arr) {
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 }
-console.log(addThemAll(2,4)); // 6*
-console.log(addThemAll(1,2,3,4)); // 10*
-console.log(addThemAll(5,5,10)); // 20*
+console.log(addThemAll(2, 4)); // 6*
+console.log(addThemAll(1, 2, 3, 4)); // 10*
+console.log(addThemAll(5, 5, 10)); // 20*
