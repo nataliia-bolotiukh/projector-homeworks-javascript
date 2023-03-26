46bd2dbede0fef314ca3ebe002ef7b02e49add77
nataliia-bolotiukh <126393745+nataliia-bolotiukh@users.noreply.github.com>
2023-03-26 17:30:43 +0200

    Create task-3.js

---
+++ lesson-7-functions/task-3.js
@@ -0,0 +1,40 @@
+const movies = [
+  {
+    movieName: "The Thing",
+    releaseYear: 1982,
+    directedBy: "Carpenter",
+    runningTimeInMinutes: 109,
+  },
+  {
+    movieName: "Aliens",
+    releaseYear: 1986,
+    directedBy: "Cameron",
+    runningTimeInMinutes: 137,
+  },
+  {
+    movieName: "Men in Black",
+    releaseYear: 1997,
+    directedBy: "Sonnenfeld",
+    runningTimeInMinutes: 98,
+  },
+  {
+    movieName: "Predator",
+    releaseYear: 1987,
+    directedBy: "McTiernan",
+    runningTimeInMinutes: 107,
+  },
+];
+function byProperty(property, direction) {
+  return (a, b) => {
+    if (a[property] < b[property]) {
+      return direction === "<" ? -1 : 1;
+    }
+    if (a[property] > b[property]) {
+      return direction === "<" ? 1 : -1;
+    }
+    return 0;
+  };
+}
+console.log(movies.sort(byProperty('releaseYear', '>'))); // виведе масив фільмів посортованих по року випуску, від старішого до новішого*
+console.log(movies.sort(byProperty('runningTimeInMinutes', '<'))); // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого*
+console.log(movies.sort(byProperty('movieName', '>'))); // виведе масив фільмів посортованих по назві, в алфавітному порядку*
